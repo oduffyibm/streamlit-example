7564b3129888e1eebc4439a534e02c5d3e47b24b
oduffyibm <136842943+oduffyibm@users.noreply.github.com>
2024-11-27 12:13:44 -0600

    Update second_app.py

--- second_app.py
+++ second_app.py
@@ -3,4 +3,37 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
+st.title("This app is awesome")
+st.subheader("And this is the subheader")
+
 st.write("Hello **world**")
+st.write_stream("The quick brown fox jumps over the lazy dog")
+
+st.divider()
+
+Hello again
+
+st.markdown("Hello **World** in markdown")
+
+
+st.write("The code for this app is")
+st.code("import altair as alt
+import numpy as np
+import pandas as pd
+import streamlit as st
+
+st.title("This app is awesome")
+st.subheader("And this is the subheader")
+
+st.write("Hello **world**")
+st.write_stream("The quick brown fox jumps over the lazy dog")
+
+Hello again")
+
+
+
+
+
+
+
+
